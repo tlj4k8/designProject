733f48b8c69bff11ba333a42cc177fa20d4ac085
Taylor Jackson <32742146+tlj4k8@users.noreply.github.com>
2018-09-17 23:12:24 -0500

    Revert "views and components added with minimal design"

--- src/router.js
+++ src/router.js
@@ -1,39 +1,15 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Home from './views/Home';
-import ClientPage from './views/ClientPage';
-import Dashboard from './views/Dashboard';
-import ChefPage from './views/ChefPage';
-import MenuPage from './views/MenuPage';
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/client',
-      name: 'ClientPage',
-      component: ClientPage
-    },
-    {
-      path: '/dash',
-      name: 'Dashboard',
-      component: Dashboard
-    },
-    {
-      path: '/chef',
-      name: 'ChefPage',
-      component: ChefPage
-    },
-    {
-      path: '/menu',
-      name: 'MenuPage',
-      component: MenuPage
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
