d8b9338a78b30af577fecb8c2f6c68c24f12519f
tlj4k8 <tjtay2008@gmail.com>
2018-09-17 23:05:00 -0500

    views and components added with minimal design

---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="bar">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavBar"
+}    
+</script>
+
+<style scope>
+.bar {
+  display: flex;
+  position: fixed;
+  top: 0;
+  z-index: 15;
+  width: 100%;
+  height: 5em;
+  background: grey;
+}
+</style>
