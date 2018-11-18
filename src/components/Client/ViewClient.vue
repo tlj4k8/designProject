c9f7ae29ae71f42548a905235aed8d9481b4ccf3
Taylor Jackson <tjtay2008@gmail.com>
2018-11-17 23:15:45 -0600

    remove visit stuff on view client page

--- src/components/Client/ViewClient.vue
+++ src/components/Client/ViewClient.vue
@@ -382,14 +382,6 @@ export default {
         'NE', 'NY', 'NV', 'NH', 'NJ', 'NM','NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI',
         'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'
         ],
-        visit: null,
-        visitDates: [
-            { value: 'client1', text: 'Client1' },
-            { value: 'client2', text: 'client2' },
-            { value: 'client3', text: 'client3' },
-            { value: 'client4', text: 'client4' },
-            { value: 'client5', text: 'client6' }
-        ],
         show: true
     }
     },
