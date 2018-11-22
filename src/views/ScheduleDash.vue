8f5db260f784c004599d1b08efffb4b9c74fbbf5
Taylor Jackson <tjtay2008@gmail.com>
2018-11-21 19:00:35 -0600

    misspellings

--- src/views/ScheduleDash.vue
+++ src/views/ScheduleDash.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="scheduleDash">
-    <h1>Menu Dashboard</h1>
+    <h1>Schedule Dashboard</h1>
     <b-button-group vertical class="dashGroup">
         <b-button to="/viewSchedule">View Schedule</b-button>
         <b-button v-if="isAdmin=='True' || isMenu=='True'" to="/scheduleNew">Add Schedule</b-button>
