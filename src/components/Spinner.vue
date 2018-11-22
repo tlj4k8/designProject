53c80c702625228da9758aac890516231c5616bb
Taylor Jackson <tjtay2008@gmail.com>
2018-11-21 23:08:19 -0600

    spinner component created

---
+++ src/components/Spinner.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="loading">
+            <div class="spinner">
+                <circle2></circle2>
+            </div>
+    </div>
+</template>
+<script>
+import {Circle2} from 'vue-loading-spinner';
+export default {
+    name: 'Spinner',
+    components:{
+      Circle2
+    }
+}
+</script>
+<style scoped>
+.loading{
+    position: fixed;
+    top: 0;
+    height: 100vh;
+    width: 100vw;
+    background: rgb(136, 136, 136);
+    overflow: auto;
+    z-index: 3;
+    opacity: 0.80;
+    display: flex;
+    justify-content: center;
+}
+.spinner{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-self: center;    
+}
+.spinner.spinner--circle-2{
+    width: 100px !important;
+    height: 100px !important;
+    border-width: 10px !important;
+}
+</style>
